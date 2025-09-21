876cc83b66a02d48164cf3c5b9704758eb941d89
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-09-21 15:03:29 +0530

    Update flink.py

--- plugins/flink.py
+++ plugins/flink.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from database.database import db
 from helper_func import encode, decode
-from config import OWNER_ID, 
+from config import OWNER_ID,CHANNEL_ID 
 import logging
 
 logger = logging.getLogger(__name__)
